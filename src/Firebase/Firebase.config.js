9ac2bb17bbdc538327008b293a64b12ba4bc8e1c
Mohammad Roni <ronichowdhury999@gmail.com>
2024-10-12 14:56:17 +0600

    install the tailwindcss,daisyui,react-router-dom,firebase

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAJQrchzpM1L3oY2vTWAK1ehbKXR-micmU",
+    authDomain: "dragon-news-7f830.firebaseapp.com",
+    projectId: "dragon-news-7f830",
+    storageBucket: "dragon-news-7f830.appspot.com",
+    messagingSenderId: "1014649337811",
+    appId: "1:1014649337811:web:b2a485e18996d21057e126"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export default auth;
