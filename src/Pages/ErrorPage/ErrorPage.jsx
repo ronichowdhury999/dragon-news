64ae7080ee17a50305c4913b02802452fe06feab
Mohammad Roni <ronichowdhury999@gmail.com>
2024-10-16 19:55:02 +0600

    errorPages added

---
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,10 @@
+
+const ErrorPage = () => {
+  return (
+    <div className="flex justify-center items-center  bg-sky-100 w-full h-screen">
+        <h3 className="text-7xl font-bold">Opps</h3>
+    </div>
+  )
+}
+
+export default ErrorPage
--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -7,11 +7,13 @@ import Login from "../Pages/Login/Login"
 import Register from "../Pages/Register/Register"
 import NewsDetails from "../Pages/NewsDetails/NewsDetails"
 import PrivateRoute from "../PrivateRoute/PrivateRoute"
+import ErrorPage from "../Pages/ErrorPage/ErrorPage"
 
 const Router = createBrowserRouter([
     {
         path: '/',
         element: <Root />,
+        errorElement:<ErrorPage/>,
         children: [
             {
                 path: '/',
